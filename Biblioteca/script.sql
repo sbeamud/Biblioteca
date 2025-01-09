4cf549ea30cfc88e550e308c40a931b78324b088
sbeamud <sergiobeamud@iesrioarba.es>
2025-01-09 01:39:15 +0100

    Corrijo script

--- Biblioteca/script.sql
+++ Biblioteca/script.sql
@@ -1,5 +1,3 @@
--- Active: 1733823502192@@127.0.0.1@3306@biblioteca
-CREATE DATABASE IF NOT EXISTS biblioteca;
 USE biblioteca;
 
 CREATE TABLE Libros (
@@ -7,13 +5,13 @@ CREATE TABLE Libros (
     titulo VARCHAR(150) NOT NULL,
     autor VARCHAR(150) NOT NULL,
     disponible BOOLEAN DEFAULT TRUE
-)
+);
 
 CREATE TABLE Usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL
-)
+);
 
 CREATE TABLE Prestamos (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -22,4 +20,4 @@ CREATE TABLE Prestamos (
     id_usuario INT NOT NULL,
     FOREIGN KEY (id_libro) REFERENCES Libros(id),
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id) ON DELETE CASCADE
-)
+);
